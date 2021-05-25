f51cae0d3977d8cb750f79d1892813b8d6c460d1
diego221 <43753823+diego221@users.noreply.github.com>
2021-05-25 17:56:01 +0200

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "login" {
+    type = string
+    description = "Administrator user for SQL Server:"
+}
+
+variable "login_password" {
+    type = string
+    description = "Password must meet Azure complexity requirements:"
+}
